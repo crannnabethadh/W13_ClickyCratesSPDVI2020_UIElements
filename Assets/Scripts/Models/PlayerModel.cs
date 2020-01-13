7179acbc40e566a58f73ac6bc94f5ae4a88cfb70
unknown <crannnabethadh@outlook.es>
2020-01-13 16:43:18 +0100

    Starting to implement the login screen. Still not working. Fighting with flexbox and Yoga layout.

---
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class PlayerInfo
+{
+    public string Id;
+    public string Email;
+    public string FirstName;
+    public string LastName;
+    public string NickName;
+    public string City;
+    public string BlobUri;
+}
---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Unity.UIElements.Runtime;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    public PanelRenderer m_LoginScreenPanel;  // Important! Remember to tick Enable live updates!!!!
+    private TextField spdvi_FirstNameTextField;
+    private TextField spdvi_LastNameTextField;
+
+    private void OnEnable()
+    {
+        m_LoginScreenPanel.postUxmlReload = BindGameScreen;  // Important!!!
+    }
+
+    private IEnumerable<Object> BindGameScreen()
+    {
+
+        var root = m_LoginScreenPanel.visualTree;
+        spdvi_FirstNameTextField = root.Query<TextField>("spdvi_FirstNameTextField");  // name of the element in UXML
+        spdvi_LastNameTextField = root.Query<TextField>("spdvi_LastNameTextField");  // name of the element in UXML
+        return null;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
